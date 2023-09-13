b12ff9dee67884a61dff604ce58a1a8bb644b1f9
lakshitgosain <lakshit.gosain@gmail.com>
2023-09-13 21:44:26 +0530

    Add files via upload

---
+++ friends-by-age.py
@@ -0,0 +1,39 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
+
+lines = sc.textFile("file:///C:/Users/dell/Desktop/SparkCourse/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] // x[1])
+results = averagesByAge.collect()
+for result in results:
+    print(result)
+    
+# from pyspark import SparkConf, SparkCotext
+
+# conf=SparkConf().setMaster("local").setAppName("Friend_avg")
+# sc=SparkContext(conf=conf)
+
+
+# def parseLine(line):
+    
+#     fields=line.split(',')
+#     age=int(fields[2])
+#     numFriends=int(fields[3])
+#     return (age,numFriends)
+
+# lines=sc.textFile("")
+# rdd=lines.map(parseLine)
+# totalsByAge=rdd.reduceByValues(lambda x:(x,1)).reduceByKey(lambda x,y : x[0]+y[0], x[1]+y[1])
+# averagesByAge=totalsByAge.mapValues(lambda x: x[0]//x[1])
+# results=averagesByAge.collect()
+#     for result in results:
+# print(result)
